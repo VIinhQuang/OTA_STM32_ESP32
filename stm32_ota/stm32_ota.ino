7667ca95bfc08645a8cfdc119a6e7c567ffbc0c2
Vinh Quang <vinhquang4220@gmail.com>
2023-12-14 07:04:39 +0700

    update

--- stm32_ota/stm32_ota.ino
+++ stm32_ota/stm32_ota.ino
@@ -1,35 +1,36 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-//#include <Arduino_JSON.h>
-#include <TBPubSubClient.h>
-#include <ESP32httpUpdate.h>
-#include <WiFiClient.h>
-#include <math.h>
-#include <string>
-#include <FS.h>
 #include "stm32ota.h"
-#include <SPIFFS.h>
-#include <ArduinoJson.h>
-
-
-unsigned long lastTime;
-char link_bin[100];
-const char* ssid = "Lau 1 2B 2.5G";
-const char* password = "dangky4G";
+#include <ESP8266WiFi.h>
+//#include <WiFiClient.h>
+//#include <ESP8266WebServer.h>
+//#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+#include "ArduinoJson.h"  //Version 6.15.2
 #define ARDUINOJSON_USE_LONG_LONG 1
+
+SoftwareSerial Debug;  //For debug only
+#define MYPORT_TX 14   //For debug only
+#define MYPORT_RX 13   //For debug only
+
 stm32ota STM32(5, 4, 2);  //For use with libray STM32OTA
+const char* ssid = "HardwareLab";
+const char* password = "Hayuongbidao";
 
-const char* link_Updt  = "https://raw.githubusercontent.com/VIinhQuang/OTA_STM32_ESP32/main/LinkUpdt.txt";
 
+const char* link_Updt = link_Updt = "https://raw.githubusercontent.com/VIinhQuang/OTA_STM32_ESP32/main/LinkUpdt.txt";
+char link_bin[100];
 boolean MandatoryUpdate = false;
 //----------------------------------------------------------------------------------
-const int buttonPin = 21;
+const int buttonPin = 12;
 const int ledPin = 2;
 boolean aux = false;
-
+unsigned long lastTime;
 int button = true;
+
+//----------------------------------------------------------------------------------
 void wifiConnect() {
-  Serial.println("");
+  Debug.println("");
   WiFi.disconnect(true);  
   WiFi.mode(WIFI_STA);
   delay(2000);  //Aguarda a estabilizaçao do modulo.
@@ -37,14 +38,14 @@ void wifiConnect() {
   byte b = 0;
   while (WiFi.status() != WL_CONNECTED && b < 60) {  //Tempo de tentativa de conecxao - 60 segundos
     b++;
-    Serial.print(".");
+    Debug.print(".");
     delay(500);
   }
-  Serial.println("");
-  Serial.print("IP:");
-  Serial.println(WiFi.localIP());
+  Debug.println("");
+  Debug.print("IP:");
+  Debug.println(WiFi.localIP());
 }
-
+//----------------------------------------------------------------------------------
 void checkupdt(boolean all = true) {
   if (WiFi.status() != WL_CONNECTED) {
     return;
@@ -60,7 +61,7 @@ void checkupdt(boolean all = true) {
   s.trim();
 
   if (all) {
-    Serial.println(s);  //usar apenas no debug
+    Debug.println(s);  //usar apenas no debug
   }
 
 
@@ -72,12 +73,15 @@ void checkupdt(boolean all = true) {
   deserializeJson(doc, s);
   strlcpy(link_bin, doc["link"] | "", sizeof(link_bin));
   MandatoryUpdate = doc["mandatory"] | false;
-  Serial.println(link_bin);  //For debug only
+  Debug.println(link_bin);  //For debug only
   //Debug.println(MandatoryUpdate);                   //For debug only
 }
+
+//----------------------------------------------------------------------------------
 void setup() {
-  Serial.begin(9600);
-  Serial.println("DEBUG SOFTWARESERIAL");
+  Debug.begin(9600, SWSERIAL_8N1, MYPORT_RX, MYPORT_TX, false);  //For debug only
+  Serial.begin(9600, SERIAL_8E1);
+  Debug.println("DEBUG SOFTWARESERIAL");
   SPIFFS.begin();
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
@@ -87,19 +91,20 @@ void setup() {
   //STM32.RunMode();
   
   checkupdt();
-  Serial.println("END OF INITIALIZATION");
+  Debug.println("END OF INITIALIZATION");
 }
+
 void loop() {
 
   button = digitalRead(buttonPin);
   if (!button) {
     digitalWrite(ledPin, HIGH);
-    Serial.println("START UPDATE");
+    Debug.println("START UPDATE");
     delay(2000);
     checkupdt(false);
     String myString = String(link_bin);
-    Serial.println(STM32.otaUpdate(myString));  //For debug only
-    Serial.println("END OF UPDT");              //For debug only
+    Debug.println(STM32.otaUpdate(myString));  //For debug only
+    Debug.println("END OF UPDT");              //For debug only
   }
   //_______________________________________________________________________
 
